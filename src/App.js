7d5c88b0a85c793706a284205378d6e69b05bb04
Shahin <shahinhaque07@gmail.com>
2023-01-28 09:16:19 +0100

    simple modal with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => setOpenModal(true)}
+        className="show_modal App-header"
+        aria-controls="Modal"
+        aria-expanded={openModal}
+      >
+        Show Modal
+      </button>
+
+      {openModal && (
+        <div className="modal" aria-label="Modal">
+          <button onClick={() => setOpenModal(false)} className="close_modal">
+            X
+          </button>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis
+            aperiam culpa id? Et culpa animi eaque similique rem nisi dolores,
+            nulla voluptate? Ipsum laboriosam est quo, minima eligendi dolores
+            assumenda. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatem hic ducimus expedita non eligendi? Excepturi ratione, ad
+            quisquam similique corporis porro quidem odio accusantium, earum ab,
+            ut dolores optio repellendus.
+          </p>
+        </div>
+      )}
     </div>
   );
 }
